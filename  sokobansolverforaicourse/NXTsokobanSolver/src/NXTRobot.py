343fefb63b05c3f61ab54e1adcea771042307349
morten.s.laursen <morten.s.laursen@cfb6f8ac-9702-11de-a1ee-bbbdd2d7798b>
2009-09-08 08:45:59 +0000

    virtual first release

---  sokobansolverforaicourse/NXTsokobanSolver/src/NXTRobot.py
+++  sokobansolverforaicourse/NXTsokobanSolver/src/NXTRobot.py
@@ -117,10 +117,32 @@ class Movement():
             self.physicalMover.move_up()
         if self.virtualMover != None:
             self.virtualMover.move_up()
+    def move_left(self):
+        if self.physicalMover != None:
+            self.physicalMover.move_left()
+        if self.virtualMover != None:
+            self.virtualMover.move_left()     
+    def move_right(self):
+        if self.physicalMover != None:
+            self.physicalMover.move_right()
+        if self.virtualMover != None:
+            self.virtualMover.move_right()   
+    def move_down(self):
+        if self.physicalMover != None:
+            self.physicalMover.move_down()
+        if self.virtualMover != None:
+            self.virtualMover.move_down()   
 
 class Mover():
     def move_up(self):
         raise "not implemented"
+    def move_down(self):
+        raise "not implemented"
+    def move_left(self):
+        raise "not implemented"
+    def move_right(self):
+        raise "not implemented"
+
     
 class MovementPhysical(Mover):
     def move_up(self):
@@ -130,14 +152,44 @@ class MovementVirtual(Mover):
     def __init__(self, map):
         self.map = map
     def move_up(self):
+        jewel = self.map.check_for_jewel(self.map.man[0].x,self.map.man[0].y-1)
+        if jewel != None:
+            jewel.y = jewel.y-1
         self.map.man[0].y = self.map.man[0].y-1
-        print 'moving'
+        self.map.man[0].img = self.map.man[0].img_up
+        print 'moving up'
+    def move_down(self):
+        jewel = self.map.check_for_jewel(self.map.man[0].x,self.map.man[0].y+1)
+        if jewel != None:
+            jewel.y = jewel.y+1
+        self.map.man[0].y = self.map.man[0].y+1
+        self.map.man[0].img = self.map.man[0].img_down
+        print 'moving down'
+    def move_left(self):
+        jewel = self.map.check_for_jewel(self.map.man[0].x-1,self.map.man[0].y)
+        if jewel != None:
+            jewel.x = jewel.x-1
+        self.map.man[0].x = self.map.man[0].x-1
+        self.map.man[0].img = self.map.man[0].img_left
+        print 'moving left'
+    def move_right(self):
+        jewel = self.map.check_for_jewel(self.map.man[0].x+1,self.map.man[0].y)
+        if jewel != None:
+            jewel.x = jewel.x+1
+        self.map.man[0].x = self.map.man[0].x+1
+        self.map.man[0].img = self.map.man[0].img_right
+        print 'moving right'
         
 class Sprite():
-    def __init__(self, x,y,img):
+    def __init__(self, x,y,img, img_up=None, img_down=None, img_left=None, img_right=None):
         self.img = pygame.image.load(img).convert_alpha()
         self.x = x
         self.y = y
+        if img_up != None:
+            self.img_up = pygame.image.load(img_up).convert_alpha()
+            self.img_down = pygame.image.load(img_down).convert_alpha()
+            self.img_left = pygame.image.load(img_left).convert_alpha()
+            self.img_right = pygame.image.load(img_right).convert_alpha()
     
 class Map():
     def __init__(self, maptext):
@@ -159,7 +211,7 @@ class Map():
                 elif point == 'J':
                     self.jewels.append(Sprite(i,j,'../gfx/jewel.bmp'))
                 elif point == 'M':
-                    self.man.append(Sprite(i,j,'../gfx/man.bmp'))
+                    self.man.append(Sprite(i,j,'../gfx/man.bmp', '../gfx/man_up.bmp', '../gfx/man_down.bmp', '../gfx/man_left.bmp', '../gfx/man_right.bmp'))
                 elif point == 'G':
                     self.goals.append(Sprite(i,j,'../gfx/goal.bmp'))
 
@@ -173,6 +225,27 @@ class Map():
             return self.textfile[y+1][x]
         except:
             return ' '
+    def check_for_obstacle(self, x, y):
+        obstacle = self.check_for_brick(x,y)
+        if obstacle == None:
+            obstacle = self.check_for_jewel(x, y)
+        return obstacle
+    
+    def check_for_jewel(self, x, y):
+        for jewel in self.jewels:
+            if jewel.y == y and jewel.x == x:
+                return jewel
+        return None
+    def check_for_brick(self, x, y):
+        for brick in self.bricks:
+            if brick.y == y and brick.x == x:
+                return brick
+        return None
+    def check_for_goal(self, x, y):
+        for goal in self.goals:
+            if goal.y == y and goal.x == x:
+                return goal
+        return None
 class Painter(threading.Thread):
     def __init__(self, map):
         threading.Thread.__init__(self)
@@ -227,13 +300,19 @@ def main_temp():
     raw_input()
 def main_temp2():
     pygame.display.set_mode()
-    t = Map('/media/disk-1/SokobanTestingProgram/mymap.txt')
-    x = Movement(MovementPhysical(), MovementVirtual(t))
+    t = Map('../rsc/mymap.txt')
+    x = Movement(None, MovementVirtual(t))
     tusch = Painter(t)
     tusch.start()
  #   tusch.draw()
     time.sleep(3)
-    x.move_up()
+    x.move_right()
+    time.sleep(1)
+    x.move_right()
+    time.sleep(1)
+    x.move_right()
+    time.sleep(1)
+    x.move_down()
  #   tusch.draw()
     raw_input()
 if __name__ == '__main__':
